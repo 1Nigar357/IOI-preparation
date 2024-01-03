82cf08ddeb6682d54e7c1351c5589b240ba08287
1Nigar357 <98217625+1Nigar357@users.noreply.github.com>
2024-01-03 20:49:25 +0400

    Add files via upload

---
+++ 11-april (Prim's and Kruskal's algo)/A-(kruskal-algoritym-disjoint-set).cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<pair<int,pair<int,int> > >e;
+int n, m, a, b, c, ans, p[20001], rank_[20001];
+int u, v, w;
+
+void make_set(){
+    for(int i = 1; i <= n; i++ )
+        p[i] = i;
+}
+
+int find_set(int x){
+  if( x != p[x] )
+      p[x] = find_set(p[x]);
+  return p[x];
+}
+
+void link(int x, int y){
+    if(rank_[x] > rank_[y])
+        p[y] = x;
+    else if(rank_[x] < rank_[y])
+        p[x] = y;
+    else{
+       p[y] = x;
+       rank_[x]++;
+    }
+}
+
+void union_(int x, int y){
+    link(find_set(x), find_set(y));
+}
+
+int main()
+{
+    cin>>n>>m;
+    for(int i = 1; i <= m; i++ ){
+        cin>>a>>b>>c;
+        e.push_back({c, {a, b}});
+    }
+    sort(e.begin(), e.end());
+    make_set();
+    for(int i = 0; i < e.size(); i++){
+        u = e[i].second.first;
+        v = e[i].second.second;
+        w = e[i].first;
+        if(find_set(u) != find_set(v) ){
+            ans += w;
+            union_(u, v);
+        }
+    }
+    cout<<ans<<endl;
+}
---
+++ 11-april (Prim's and Kruskal's algo)/A-(prim-algorithm).cpp
@@ -0,0 +1,41 @@
+// MR.KANAN'S IMPLEMENTATION OF PRIM'S ALGORITHM
+
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 2000000000
+vector<pair<int, int> > g[20001];
+priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > >pq;
+int n, m, a, b, w, MST, key[20001], color[20001];
+void Prim(){
+    for(int i = 2; i <= n; i++){
+        key[i] = INF;
+    }
+    pq.push(make_pair(0, 1));
+    while(pq.size() != 0){
+        int v = pq.top().second;
+        pq.pop();
+        if(color[v] == 1){
+            continue;
+        }
+        for(int i = 0; i < g[v].size(); i++){
+            int u = g[v][i].first;
+            if(color[u] == 0 && key[u] > g[v][i].second){
+                key[u] = g[v][i].second;
+                pq.push(make_pair(key[u], u));
+            }
+        }
+        color[v] = 1;
+        MST += key[v];
+    }
+}
+int main()
+{
+    cin>>n>>m;
+    while(m--){
+        cin>>a>>b>>w;
+        g[a].push_back(make_pair(b, w));
+        g[b].push_back(make_pair(a, w));
+    }
+    Prim();
+    cout<<MST<<endl;
+}
